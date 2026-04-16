b4854904b7eec5e4c5a32e0b3343f3bd9ffe32ec
Vinicius Gehrke <viniciusgehrkedasilva@ienh.com.br>
2026-04-16 13:27:22 -0300

    Add default attributes to ItemNota model

--- app/Models/ItemNota.php
+++ app/Models/ItemNota.php
@@ -7,6 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class ItemNota extends Model
 {
     protected $table = 'itens_nota';
+    protected $attributes = [
+    'ncm' => 'N/A',
+    'codigo_sku' => 'N/A',
+    'unidade_medida' => 'UN',
+    'valor_icms' => 0,
+    'valor_desconto => 0,
+    ];
     protected $fillable = [
         'nota_fiscal_id', 'codigo_sku', 'descricao', 'quantidade',
         'unidade_medida', 'ncm', 'valor_unitario', 'valor_icms',
